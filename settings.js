e74a15c66f5245e3a3ef2bc716ed4a18d71822bf
YOUR-VILLAIN <villain002x@gmail.com>
2026-08-01 00:23:51 +0530

    ~• 𝗩 𝗜 𝗟 𝗟 𝗔 𝗜 𝗡 ༼༽

---
+++ settings.js
@@ -0,0 +1,16 @@
+const settings = {
+  packname: 'Knight Bot',
+  author: '‎𝙑 𝙄 𝙇 𝙇 𝘼 𝙄 𝙉',
+  botName: "~• 𝗩 𝗜 𝗟 𝗟 𝗔 𝗜 𝗡 ༼༽",
+  botOwner: '~• 𝗩 𝗜 𝗟 𝗟 𝗔 𝗜 𝗡 ༼༽', // Your name
+  ownerNumber: '9194752 05887', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "𝙏𝙃𝙄𝙎 𝘽𝙊𝙏 𝙄𝙎 𝘾𝙍𝙀𝘼𝙏𝙀 𝘽𝙔 𝘾𝙍𝙀𝙓",
+  version: "3.0.7",
+  updateZipUrl: "https://github.com/mruniquehacker/Knightbot-MD/archive/refs/heads/main.zip",
+};
+
+module.exports = settings;
